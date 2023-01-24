17a6b4f9c4443334c90e445001d96199c4408345
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-01-24 13:09:45 +0200

    Add queries to find animal names with specific characters and animals born in range of dates

--- queries.sql
+++ queries.sql
@@ -1,4 +1,4 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT * from animals WHERE name = 'Luna';
+SELECT name from animals WHERE date_of_birth >='2016-01-01' AND date_of_birth < '2019-01-01';
