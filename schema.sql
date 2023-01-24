ccc4ef1b115910a814ad80c421d8966b145178f5
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-01-24 12:22:01 +0200

    Add database vet_clinic and create table animals

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
-);
+    id INT PRIMARY KEY NOT NULL,
+    name varchar(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attemps INT,
+    neutered BOOLEAN,
+    wieght_kg decimal );
