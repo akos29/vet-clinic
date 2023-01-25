a34f659486cfce650faa40f31b0c3985c6959aeb
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-01-25 12:11:39 +0200

    Add data into owners table

--- data.sql
+++ data.sql
@@ -15,3 +15,13 @@ INSERT INTO animals (name,date_of_birth,escape_attempts, neutered, weight_kg) VA
 ('Boarmon','2005-06-7',7,true,-20.4),
 ('Blossom','1998-10-13',3,true,-17),
 ('Ditto','2022-05-14',4,true,-22);
+
+
+
+INSERT INTO owners (full_name,age) VALUES 
+('Sam Smith',34),
+('Jennifer Orwell',19),
+('Bob',45),
+('Melody Pond',77),
+('Dean Winchester',14),
+('Jodie Whittaker',38);
