833aeec060b03434ff77012355d53fc354d93a26
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2023-01-25 12:05:59 +0200

    Add new tables owners and species and update FK constraints for all

--- schema.sql
+++ schema.sql
@@ -1,11 +1,30 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(100) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    escape_attempts INT,
-    neutered BOOLEAN,
-    weight_kg DECIMAL );
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100) NOT NULL,
+  date_of_birth DATE NOT NULL,
+  escape_attempts INT,
+  neutered BOOLEAN,
+  weight_kg DECIMAL );
 
-    ALTER TABLE animals ADD species VARCHAR(50);
+  ALTER TABLE animals ADD species VARCHAR(50);
+
+CREATE TABLE owners (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  full_name VARCHAR(100) NOT NULL,
+  age INT );
+
+CREATE TABLE species ( 
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name VARCHAR(100) NOT NULL );
+
+  ALTER TABLE animals 
+  ADD CONSTRAINT animalsPrimaryKey PRIMARY KEY (id);
+
+  ALTER TABLE animals DROP COLUMN species;
+
+  ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
+  ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
+
+  ALTER TABLE animals ADD FOREIGN KEY (species_id) REFERENCES species(id);
