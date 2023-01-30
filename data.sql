6702ba3a2f717a484211605164a226fdf9e8e92a
Sergiy Shkolnik <sps676@gmail.com>
2023-01-30 13:41:41 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -71,6 +71,4 @@ UPDATE animals SET owner_id=5 WHERE name LIKE 'Boarmon';
 SELECT name, owner_id FROM animals;
 
 insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
-
-
-CREATE INDEX vet_id_asc ON vets(id ASC);
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
